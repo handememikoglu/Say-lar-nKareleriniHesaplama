7b4ff6196e843bf15d77eb938bba502ade66d764
Hande Eylül Memikoğlu <handememikoglu@gmail.com>
2024-12-22 11:22:30 +0300

    code is enclosed in function

--- SayilarinKareleri.js
+++ SayilarinKareleri.js
@@ -1,5 +1,11 @@
 //Sayıların Karelerini Hesaplama
-const array = [1,2,3,4,5,6,7,8,9,10];
-const newArray = array.map(num => num * num);
-console.log(newArray);
-
+function kareAlma() {
+    const array = [1,2,3,4,5,6,7,8,9,10];
+    console.log(array);
+    
+    const newArray = array.map(num => num * num);
+    console.log(newArray);
+    
+    return newArray;
+}
+kareAlma();
